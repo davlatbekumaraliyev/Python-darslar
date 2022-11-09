3698d9a03ebf58e854b7f6c179d9034bc865ecd5
davlatbekumaraliyev <117083093+davlatbekumaraliyev@users.noreply.github.com>
2022-11-09 16:13:56 +0300

    bot

--- 15dars.py
+++ 15dars.py
@@ -1,46 +1,30 @@
-# def salom_ber():
-#     """Salom beruvchi funksiya"""
-#     print("Assalomu alaykum!")
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '5414998861:AAF11mW8AA0NLmkj5ijcY8CEdwe-0Ide2O0'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+ 
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
     
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
     
-# salom_ber()
-
-
-# def salom_ber(ism, familiya):
-#     """Foydalanuvchini ismini qabul qilib,
-#     unga salom beruvchi funksiya"""
-#     print(f"Assalomu alaykum hurmatli {ism.title()} {familiya.title()}")
-    
-# salom_ber('hasan', 'hasanov')
-# salom_ber('ali', 'aliyev')
-
-
-# def toliq_ism(ism, familiya):
-#     """Foydalanuvchi ism va familiyasini jamlab chiqaruvchi funksiya"""
-#     print(
-#         f"Foydalanuvchi ismi: {ism.title()}\n"
-#         f"Foydalanuvchi familiyasi: {familiya.title()}\n"
-#         )
-    
-# toliq_ism('ali', "aliyev")
-
-
-# def yosh_hisobla(ism, tugilgan_yil):
-#     '''Yosh hisoblovchi dastur'''
-#     print(f"{ism.title()} {2022-tugilgan_yil} yoshda.")
-    
-# yosh_hisobla('ali', 1997)
-
-
-# def yosh_hisobla(t_y, j_y=2022):
-#     """Foydalanuvchi tugilgan yilidan uning yoshini hisoblaydigan dastur"""
-#     print(f"Siz {j_y-t_y} yoshdasiz")
-    
-# yosh_hisobla(2200, 2022)
-
-def talaba_malumotlari(ism, familiya, t_yil, tel_raqam, kurs):
-    """Talaba malumotlari"""
-    print(f"Talabaning ismi: {ism.title()},\nfamiliyasi: {familiya.title()},\ntugulgan yili:{t_yil},\ntelefon raqami: {tel_raqam},\n{kurs} da o'qiydi.")
-
-talaba_malumotlari('ali', 'aliyev', 2000, 99_123_45_67, 4)
-
