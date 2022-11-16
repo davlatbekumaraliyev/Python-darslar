2690325342a0a039301693ac84e8df77f3515bb5
davlatbekumaraliyev <117083093+davlatbekumaraliyev@users.noreply.github.com>
2022-11-16 16:49:53 +0300

    Uy ishi5

---
+++ Uy ishi/uy ishi5.py
@@ -0,0 +1,46 @@
+sonlar = [9, 7, 1, 4, 2, 3, 5, 8, 6]
+# def bubble(sonlar):
+#     n = len(sonlar)
+#     swaps = 0
+#     for i in range(n-1):
+#         if sonlar[i] > sonlar[i+1]:
+#             sonlar[i], sonlar[i+1] = sonlar[i+1], sonlar[i]
+#             swaps += 1
+#     return swaps
+    
+# def bubble_sort(sonlar):
+#     n = len(sonlar)
+#     for _ in range(n-1):
+#         if bubble(sonlar) == 0:
+#             break
+        
+# bubble_sort(sonlar)
+
+
+
+# def selection(sonlar):
+#     for i in range(len(sonlar)):
+#         v = i
+        
+#         for q in range(i, len(sonlar)):
+#             if sonlar[v] > sonlar[q]:
+#                 v = q
+            
+#         sonlar[i], sonlar[v] = sonlar[v], sonlar[i]
+
+# def selection_sort(sonlar):
+#     n = len(sonlar)
+#     for _ in range(n-1):
+#         if selection(sonlar) == 0:
+#             break
+        
+        
+# selection_sort(sonlar)
+
+
+
+
+
+
+
+
