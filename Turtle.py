dfe0fe3dfe103f7561227fe06e69723b70312eb8
davlatbekumaraliyev <117083093+davlatbekumaraliyev@users.noreply.github.com>
2022-11-25 17:14:33 +0300

    qiziqish

---
+++ Turtle.py
@@ -0,0 +1,26 @@
+# from turtle import *
+# speed(10)
+# color('cyan')
+# bgcolor('black')
+# b=(200)
+
+# while b>0:
+#     left(b)
+#     forward(b * 3)
+#     b = b - 1
+    
+# import turtle
+# t = turtle.Turtle()
+# s=turtle.Screen()
+# s.bgcolor('black')
+# t.width(2)
+# t.speed(20)
+# col = ('red', 'cyan', 'blue', 'green')
+
+
+# for i in range(350):
+#     t.pencolor(col[i%2])
+#     t.forward(i*4)
+#     t.right(200
+
+
