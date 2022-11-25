3de643d63a73a2cce963e1b3e5edcdeb13650687
davlatbekumaraliyev <117083093+davlatbekumaraliyev@users.noreply.github.com>
2022-11-25 17:14:38 +0300

    Create talabani testi.py

---
+++ talabani testi.py
@@ -0,0 +1,47 @@
+# import talaba_mod as tm
+# talaba1 = tm.talaba_info('ali', 'valiyev', 3, 2000, 22)
+# tm.t_i_p(talaba1)
+
+
+
+
+# import talaba_mod as tm
+
+# talaba = tm.t_k()
+
+# print(talaba)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
